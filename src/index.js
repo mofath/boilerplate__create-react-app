6af7d8393bf86fd48e1980f5de526b9c260271c6
mofath <mohamed.elgaedi@gmail.com>
2022-06-28 02:05:13 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './js/components/App';
 import 'normalize.css';
-import './scss/index.scss';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-	return <div className='App'>App</div>;
+  return <div className='App'>App</div>;
 }
 
 export default App;
